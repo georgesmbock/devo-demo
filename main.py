6536ce6410c0197922112a87a999a06f83260b93
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 17:15:08 +0100

    test2 pr

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 print("Cool")
-#import pandas as pd
-#import numpy as np
-#import requests as rq
-#from bs4 import BeautifulSoup as bs
 print(f"{9 + 9 + 2}")
 print("Lets go!")
 print("Hello, World!")
