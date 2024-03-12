61c40208d02df37aa2172b0ac911ee71a486e0e0
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 16:55:21 +0100

    test1 pr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import numpy as np
-import requests as rq
-from bs4 import BeautifulSoup as bs
+#import pandas as pd
+#import numpy as np
+#import requests as rq
+#from bs4 import BeautifulSoup as bs
 print(f"{9 + 9 + 2}")
 print("Lets go!")
 print("Hello, World!")
