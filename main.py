c017c8ed9f27702412fef4b2cb248ae715e960f5
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 14:13:27 +0100

    fichier v0

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import requests as rq
+from bs4 import BeautifulSoup as bs
+print(f"{9 + 9 + 2}")
+print("Let\'s go!")
+print("Hello, World!")
+print("voit et merci!")
